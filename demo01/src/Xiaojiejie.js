de9d48722e94a964b55d4e676e00e550ad3b0868
liuhuan086 <liuhuan086>
2020-10-26 21:33:25 +0800

    父组件向子组件传递参数

--- demo01/src/Xiaojiejie.js
+++ demo01/src/Xiaojiejie.js
@@ -1,5 +1,7 @@
 // import React, {Component} from 'react'
 import React, {Component, Fragment} from 'react'
+import './style.css'
+import XiaojiejieItem from "./XiaojiejieItem";
 
 class Xiaojiejie extends Component {
     constructor(props) {
@@ -7,7 +9,7 @@ class Xiaojiejie extends Component {
         this.state = {
             //数据驱动，那就在这里填入数据
             inputValue: "Ryan",
-            list: []
+            list: ["基础按摩", "精油推背", "全身按摩"]
             // 数据初始化完毕
         }
     }
@@ -17,17 +19,25 @@ class Xiaojiejie extends Component {
             // <div>
             <Fragment>
                 <div>
+                    {/*
+                        点击标签之后，鼠标聚焦到输入框中，
+                        使用htmlFor是为了区分JavaScript中的关键词for
+                    */}
+                    <label htmlFor={"test"}>增加服务</label>
+
                     {/* 完成上面的数据绑定 */}
                     {/* input说明状态有变化，这时就需要事件绑定，onChange*/}
                     {/*<input value={this.state.inputValue} onChange={this.inputChange}/>*/}
                     {/* 下面用this.state.inputValue = e.target.value会报错，需要进行绑定 */}
-                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
-                    <button>增加服务</button>
+                    <input id="test" className={"input"} value={this.state.inputValue}
+                           onChange={this.inputChange.bind(this)}/>
+                    <button onClick={this.addList.bind(this)}>增加服务</button>
                 </div>
 
                 <ul>
-                    <li>头部按摩</li>
-                    <li>精油推背</li>
+                    {
+                        <XiaojiejieItem />
+                    }
                 </ul>
                 {/*</div>*/}
             </Fragment>
@@ -67,6 +77,25 @@ class Xiaojiejie extends Component {
         // 同时数据的改变也不能用上面这种方式
         this.setState({
             inputValue: e.target.value
+        })
+    }
+
+    // 增加列表
+    addList() {
+        this.setState({
+            // ... 三个点表示扩展运算符，相当于把上面list中的元素全都以 点 来代替
+            list: [...this.state.list, this.state.inputValue],
+            // 通过这种方式输入框中的值就会被清空，否则会出现在输入框中
+            inputValue: ""
+        })
+    }
+
+    // 删除列表
+    DeleteItem(index) {
+        let list = this.state.list
+        list.splice(index, 1)
+        this.setState({
+            list: list
         })
     }
 }
---
+++ demo01/src/XiaojiejieItem.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import "./style.css";
+import Xiaojiejie from "./Xiaojiejie";
+
+class XiaojiejieItem extends Component {
+    // state = {}
+    render() {
+        return (
+            <li>
+                小姐姐
+            </li>
+        );
+
+    }
+
+}
+
+export default XiaojiejieItem
